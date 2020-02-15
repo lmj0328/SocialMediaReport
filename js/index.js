d661e8e342ce721093d9957f5a86709e9fc80c18
Mengjiao li <lmj0328@uw.edu>
2020-02-14 23:12:21 -0800

    added simple transition

--- js/index.js
+++ js/index.js
@@ -11,4 +11,43 @@ $(window).scroll(function() {
         $("#navigation-bar .navbar-nav li a").removeClass("scrolled");
         $("#navigation-bar .navbar-nav i").removeClass("scrolled");    
     } 
-})
+
+    var scroll = window.requestAnimationFrame || function(callback) {
+        window.setTimeout(callback,1000/60)
+    };
+
+    let revealOnScroll = document.querySelectorAll('.revealOnScroll');
+    function checkifInView() {
+        revealOnScroll.forEach((element) => {
+            if(isElementInViewport(element)) {
+                $(element).addClass('current');
+                $(element).addClass('revealed');
+            } else {
+                $(element).removeClass('current');
+                // $(element).removeClass('revealed');
+
+            }
+        })
+        
+    }
+
+    checkifInView();
+});
+
+function isElementInViewport(el) {
+    // special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+    (rect.top <= 0
+        && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+}
